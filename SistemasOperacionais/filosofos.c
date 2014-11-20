e131a6f425bca4fcea8979e62223722fc78c6dc2
DanielJunior <danieljunior@id.uff.br>
2014-11-20 13:16:48 -0200

    Problema do jantar dos filósofos com um filósofo pegando o garfo ao contrário aparentemente resolvido

--- SistemasOperacionais/filosofos.c
+++ SistemasOperacionais/filosofos.c
@@ -2,92 +2,40 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-
 #define N 5 /* Number of philosphers */
-#define DIREITA(i) (((i)+1) %N)
-#define ESQUERDA(i) (((i)==N) ? 0 : (i)+1)
+#define ESQUERDA(i) ( ((i) + N - 1) % N ) 
+#define DIREITA(i) ( ((i) + 1) % N )
 
 typedef enum { PENSANDO, COMFOME, COMENDO } estado_filosofo;
-pthread_mutex_t esq,dir;
-pthread_mutex_t garfo;
+estado_filosofo estados[N];
+pthread_mutex_t garfos[N];
 
-estado_filosofo estado[N];
-semaphore mutex =1;
-semaphore s[N]; /* one per philosopher, all 0 */
-
-void test(int i) {
-	if ( state[i] == HUNGRY &&
-		state[LEFT(i)] != EATING &&
-		state[RIGHT(i)] != EATING ) {state[i] = EATING;
-		 V(s[i]);
-	}
-}
-
-void get_forks(int i) {
-	P(mutex);
-	state[i] = HUNGRY;
-	test(i);
-	V(mutex);
-	P(s[i]);
-}
-
-void put_forks(int i) {
-	P(mutex);
-	state[i]= THINKING;
-	test(LEFT(i));
-	test(RIGHT(i));
-	V(mutex);
-}
-
-
-void pensa(id){
-	printf("Filósofo %d pensando...\n");
-}
-
-void pega_direita(id){
-
-}
-
-void pega_esquerda(id){
-	
-}
-
-void pega_garfos(id){
-	//pthread_mutex_lock(&garfo);
-	estado_filosofo[i] = COMFOME;
-	pega_direita(id);
-	pega_esquerda(id);
-	estado_filosofo[i] = COMENDO;
-}
-
-void come(id){
-	printf("Filósofo %d comendo");
-}
-
-void devolve_garfos(id){}
-
-void *filosofo(void *param) {
-    int id = (int param)
-    pensa(id);
-    pega_garfos(id);
-    come(id);
-    devolve_garfos(id);
-    pthread_exit(NULL);
-}
+void pensar(int i);
+void obterTalherEsquerda(int i);
+void obterTalherDireita(int i);
+void comer(int i);
+void devolverTalherEsquerda(int i);
+void devolverTalherDireita(int i);
+void *filosofo(void *param);
+void *filosofo_diferente(void *param);
 
 int main(void) {
-    int n;
-    pthread_mutex_init(&garfo, NULL);
-    printf("Quantidade de threads: ");
-    scanf("%d", &n);
-    pthread_t filosofos[n];
-    long i;
-    //cria as threads
-    for (i = 0; i < n; i++) {
-        pthread_create(&filosofos[i], NULL, filosofo, (void *) i);
+    pthread_t filosofos[N];
+    int i;
+    //inicializa os mutexes
+    for(i = 0; i < N; i++){
+    	pthread_mutex_init(&garfos[i], NULL);	
     }
+    //cria as threads
+    for (i = 0; i < N; i++) {
+    	if(i == (N-1)){
+    		pthread_create(&filosofos[i], NULL, filosofo_diferente, (void *) i);
+    	}else{
+        	pthread_create(&filosofos[i], NULL, filosofo, (void *) i);
+    	}
+	}
     //força a thread principal esperar o término das outras
-    for(i=0; i < n; i++){
+    for(i=0; i < N; i++){
       pthread_join( filosofos[i], NULL);
    }
 
@@ -95,5 +43,55 @@ int main(void) {
     return 0;
 }
 
+void *filosofo(void *param) {
+	int i = (int) param;
+   	pensar(i);
+	obterTalherEsquerda(i);
+	printf("Filósofo %d está com fome\n", i);
+	obterTalherDireita(i);
+	comer(i);
+	devolverTalherEsquerda(i);
+	devolverTalherDireita(i);
+	printf("Filósofo %d terminou de comer\n", i);
+	pthread_exit(NULL);
+}
 
+void *filosofo_diferente(void *param){
+	int i = (int) param;
+   	pensar(i);
+	obterTalherDireita(i);
+	printf("Filósofo %d está com fome\n", i);
+	obterTalherEsquerda(i);
+	comer(i);
+	devolverTalherDireita(i);	
+	devolverTalherEsquerda(i);
+	printf("Filósofo %d terminou de comer\n", i);
+	pthread_exit(NULL);
+}
 
+void pensar(int i){
+	estados[i] = PENSANDO;
+	printf("Filósofo %d está pensando...\n",i);
+}
+
+void obterTalherEsquerda(int i){
+	pthread_mutex_lock(&garfos[ESQUERDA(i)]);
+	estados[i] = COMFOME;
+}
+
+void obterTalherDireita(int i){
+	pthread_mutex_lock(&garfos[DIREITA(i)]);
+}
+
+void comer(int i){
+	estados[i] = COMENDO;
+	printf("Filósofo %d está comendo\n",i);
+
+}
+void devolverTalherEsquerda(int i){
+	pthread_mutex_unlock(&garfos[ESQUERDA(i)]);
+}
+void devolverTalherDireita(int i){
+	pthread_mutex_unlock(&garfos[DIREITA(i)]);
+	estados[i] = PENSANDO;	
+}
